ba5516818026af3582152533ee08655c2dfb107d
Nely Montes <njmontesdias@gmail.com>
2024-05-28 21:14:36 -0500

    Practica base de datos mongodb

---
+++ db.js
@@ -0,0 +1,12 @@
+import { MongoClient, ServerApiVersion } from 'mongodb';
+const uri = "mongodb+srv://njmontesdias:Kamillapa.2@curso-full-stack.itzlpfu.mongodb.net/?retryWrites=true&w=majority&appName=Curso-Full-Stack";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+export default client
--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 console.log('Hola mundo con Node JS')
 
 
-const express = require('express')
+import express from 'express'
 const app = express()
 const port = 3000
+import client from './db.js'
 
 //------------------Endpoint--------------------------
 
@@ -13,13 +14,81 @@ const port = 3000
 //- El cllback recibe dos parámetros 
 //- req (request o petición)
 //- res (response o la respuesta)
-app.get('/', (req, res) =>{
-res.send('Hello Wrold!')
+app.get('/api/v1/usuarios', async (req, res) => {
+
+    await client.connect()
+    const db = client.db('sample_mflix')
+    const  users = db.collection('users')
+
+    const listaUsuarios = await users.find({}).toArray()
+    console.log(listaUsuarios)
+    // const respuesta = {
+    //     mensaje: "hola"
+    // }
+
+    // res.json(respuesta)
+
+    res.json({
+        mensaje: 'lista de usuarios',
+    })
 })
- 
+
+app.get('/api/v1/usuarios/:cedula', (req, res) => {
+
+    console.log(req.params)
+    const cedula = req.params.cedula
+
+    res.json({
+        mensaje: `usuario obtenido con la cedula: ${cedula}`
+    })
+})
+
+// post: crear datos
+app.post('/api/v1/usuarios', (req, res) => {
+
+    res.json({
+        mensaje: 'usuario guardado'
+    })
+})
+
+// put: actualizar todos los
+// datos de un elemento
+app.put('/api/v1/usuarios/:cedula', (req, res) => {
+
+    const cedula = req.params.cedula
+
+    res.json({
+        mensaje: `usuario con cedula ${cedula} actualizado`
+    })
+})
+
+// patch: actualiza algunos campos
+// de nuestro elemetno
+app.patch('/api/v1/usuarios/:cedula', (req, res) => {
+
+    const cedula = req.params.cedula
+
+    res.json({
+        mensaje: `edad del usuario con cedula ${cedula} actualizada`
+    })
+})
+
+// delete: eliminar un elemento
+app.delete('/api/v1/usuarios/:cedula', (req, res) => {
+
+    const cedula = req.params.cedula
+
+    res.json({
+        mensaje: `usuario con cedula ${cedula} eliminado`
+    })
+})
+
 // Le indicamos a nuestra API que empiece a escuchar peticiones
 // en el puerto 3000 y cuando se encienda nos muestre el mensaje 
 // que hay en el consol.log....
 app.listen(port, () =>{
 console.log(`La Api está escuchando en el puerto ${port}`)
+
+
 })
+
